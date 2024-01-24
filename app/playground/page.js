8054e826de12ed7af0594ab27f22a2168709dd71
EliasenJason <eliasenjason@gmail.com>
2024-01-23 22:16:32 -0700

    again

--- app/playground/page.js
+++ app/playground/page.js
@@ -1,7 +1,7 @@
 "use client"
 import { useState, useEffect } from "react"
 
-export default function page() {
+export default function Page() {
     const [count, setCount] = useState(0);
 
   useEffect(() => {
